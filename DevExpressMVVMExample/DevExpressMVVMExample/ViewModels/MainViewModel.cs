0ffc07c038c24a3015937d65e3c9e4c273346420
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-02-19 01:24:50 +0900

    feat : add mvvm codegenerator example

---
+++ DevExpressMVVMExample/DevExpressMVVMExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace DevExpressMVVMExample.ViewModels
+{
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        [GenerateProperty]
+        int _Count;
+
+        [GenerateCommand]
+        void Increse() => Count++;
+
+        [GenerateCommand]
+        void Decrease() => Count--;
+
+        [GenerateCommand]
+        void Initialize() => Count = 0;
+        bool CanInitialize() => Count > 10;
+
+        [GenerateCommand(AllowMultipleExecution = true)]
+        async Task AsyncIncrese()
+        {
+            await Task.Delay(1000);
+            Count++;
+        }
+    }
+}
